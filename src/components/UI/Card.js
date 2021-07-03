6ca04c4931ccd6eea05c8f316bf9997022a9194e
vijay-programs <vijaykumar.k64@gmail.com>
2021-07-03 19:10:37 +0530

    refactored to arrow functions

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,6 +1,6 @@
 import "./Card.css";
 
-function Cards(props) {
+const Cards = (props) => {
   const classes = 'card ' + props.className;
   return <div className={classes}>{props.children}</div>;
 }
