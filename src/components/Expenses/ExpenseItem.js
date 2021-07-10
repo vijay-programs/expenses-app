aa851d958e830fff6e8a37a852e3e43a2eac7452
vijay-programs <vijaykumar.k64@gmail.com>
2021-07-10 16:35:05 +0530

    react event handling and useState hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,26 @@
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
+import { useState } from "react";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const click = () => {
+    setTitle("updated");
+    console.log(title)
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={click}>change title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
